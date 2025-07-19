004b37ffa89d2aff7a676d5054b6bdee18730e0b
lnit <6626484+lnit@users.noreply.github.com>
2025-07-19 18:21:27 +0900

    Config移植漏れ対応

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,8 @@ module LnigamesServer
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.auth = config_for(:auth)
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
