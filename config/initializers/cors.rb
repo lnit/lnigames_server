afbe0b04013b2394b29ed59bbc57b70c4e6c5199
lnit <6626484+lnit@users.noreply.github.com>
2025-07-21 15:44:31 +0900

    CORS設定調整

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost", "unityroom.com"
+    origins /\Ahttp:\/\/localhost(:\d+)?\z/, "unityroom.com"
 
     resource "*",
       headers: :any,
