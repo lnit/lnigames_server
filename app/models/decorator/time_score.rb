c8228fe82026c197e1901c8e802bf5c8925a0f5a
lnit <6626484+lnit@users.noreply.github.com>
2025-08-03 18:28:47 +0900

    時間が正確に表示されない問題を修正

--- app/models/decorator/time_score.rb
+++ app/models/decorator/time_score.rb
@@ -1,9 +1,8 @@
 module Decorator
   module TimeScore
     def score_txt
-      # ミリ秒を "分:秒.ミリ秒" 形式にフォーマット
-      # 例: 123456ms -> 02:03.456
-      Time.at(score / 1000.0).strftime("%M:%S.") + format("%03d", score % 1000)
+      # float型を "分:秒.ミリ秒" 形式にフォーマット
+      Time.at(score).strftime("%M:%S.%3N")
     end
   end
 end
