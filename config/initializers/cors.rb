f6155e2c0a30d6c08ded8e6a93bf3a1c5aaf4b81
lnit <6626484+lnit@users.noreply.github.com>
2025-12-09 01:59:38 +0900

    CORS許可設定を追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /\Ahttp:\/\/localhost(:\d+)?\z/, /\A.*[.]unityroom.com\z/
+    origins /\Ahttp:\/\/localhost(:\d+)?\z/, /\A.*\.unityroom.com\z/, /\A.*\.lnilab\.net\z/, /\A.*\.developer-lni\.workers\.dev\z/
 
     resource "*",
       headers: :any,
